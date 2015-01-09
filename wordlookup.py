8260ef779d4b94060b6c24ed7f010427a2173edf
Yaakov Freedman <yaakovfreedman@yahoo.com>
2015-01-08 22:24:16 -0500

    changed output to display search term

--- wordlookup.py
+++ wordlookup.py
@@ -25,12 +25,14 @@ def word_lookup(word):
     entries = doc.xpath('//entry_list/entry')
     suggestions = doc.xpath('//entry_list/suggestion')
     if entries:
+        print '\n\tInput: ' + word
         for entry in entries:
             print '========================='
             print entry.find('ew').text
             for definition in entry.xpath('def/dt'):
                 etree.strip_tags(definition,"*")
                 print '=> ' + definition.text.replace(':','', 1)
+        print '========================='
     elif suggestions:
         print 'The word "' + word + '" isn\'t in the dictionary.\nSuggestions:'
         for suggestion in suggestions:
